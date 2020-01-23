3b7ede80a19f3b2a98b748830365a3cfda2327a5
Tehreem <tehreemfazili5@gmail.com>
2020-01-23 15:03:27 +0530

    BiometricAuth for lower versions

--- app/src/main/java/com/example/biometricauth/BiometricPrompt.kt
+++ app/src/main/java/com/example/biometricauth/BiometricPrompt.kt
@@ -2,7 +2,19 @@ package com.example.biometricauth
 
 import android.annotation.TargetApi
 import android.content.Context
+import android.hardware.fingerprint.FingerprintManager
 import android.os.Build
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.util.Log
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+import androidx.core.os.CancellationSignal
+import java.io.IOException
+import java.security.GeneralSecurityException
+import java.security.KeyStore
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
 
 @SuppressWarnings("deprecation")
 @TargetApi(Build.VERSION_CODES.M)
@@ -10,6 +22,126 @@ internal class BiometricPrompt(
     private var context: Context,
     private var builder: Builder
 ) {
+    companion object {
+        private const val TAG = "BiometricPrompt"
+        private val KEY_NAME = UUID.randomUUID().toString()
+        private const val KEY_STORE_PROVIDER = "AndroidKeyStore"
+    }
+
+    private var keyStore: KeyStore? = getKeystore()
+    private var cipher: Cipher? = null
+    private var cryptoObject: FingerprintManagerCompat.CryptoObject? = null
+   // private var dialog: BiometricDialog? = null
+
+    private fun getKeystore() = try {
+        KeyStore.getInstance(KEY_STORE_PROVIDER)?.apply { load(null) }
+    } catch (e: GeneralSecurityException) {
+        Log.e(TAG, e.message)
+        null
+    } catch (exc: IOException) {
+        exc.printStackTrace()
+        null
+    }
+
+    private fun initCipher(): Boolean {
+        try {
+            val transformation =
+                "${KeyProperties.KEY_ALGORITHM_AES}/${KeyProperties.BLOCK_MODE_CBC}/${KeyProperties.ENCRYPTION_PADDING_PKCS7}"
+            cipher = Cipher.getInstance(transformation)
+
+            keyStore?.let {
+                it.load(null)
+                val key = it.getKey(KEY_NAME, null) as SecretKey
+                cipher?.init(Cipher.ENCRYPT_MODE, key) ?: return false
+                return true
+            } ?: return false
+        } catch (e: KeyPermanentlyInvalidatedException) {
+            return false
+        } catch (e: GeneralSecurityException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: IOException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        }
+    }
+    fun authenticate(authListener: BiometricPromptCompat.OnAuthenticateListener) {
+        if (initCipher()) {
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+                val cryptoObject = FingerprintManager.CryptoObject(cipher ?: return)
+                val fingerprintManager = context.getSystemService(FingerprintManager::class.java)
+                fingerprintManager.authenticate(cryptoObject, android.os.CancellationSignal(), 0,  object : FingerprintManager.AuthenticationCallback() {
+                    override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?) {
+                        super.onAuthenticationError(errMsgId, errString)
+                      //  dialog?.setStatus(errString?.toString(), R.color.colorError)
+                        authListener.onAuthError(errMsgId, errString ?: "")
+                    }
+
+                    override fun onAuthenticationSucceeded(result: FingerprintManager.AuthenticationResult?) {
+                        super.onAuthenticationSucceeded(result)
+//                        dialog?.dismiss()
+                        authListener.onAuthSuccess()
+                    }
+
+                    override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) {
+                        super.onAuthenticationHelp(helpMsgId, helpString)
+//                        dialog?.setStatus(helpString?.toString())
+                        authListener.onAuthHelp(helpMsgId, helpString ?: "")
+                    }
+
+                    override fun onAuthenticationFailed()  {
+                        super.onAuthenticationFailed()
+//                        dialog?.setStatus(context.getString(R.string.fingerprint_not_recognized), R.color.colorError)
+                        authListener.onAuthFailure()
+                    }
+                }, null)
+            } else {
+                cryptoObject = FingerprintManagerCompat.CryptoObject(cipher ?: return)
+                val fingerprintManagerCompat = FingerprintManagerCompat.from(context)
+                fingerprintManagerCompat.authenticate(cryptoObject, 0, CancellationSignal(),
+                    object : FingerprintManagerCompat.AuthenticationCallback() {
+                        override fun onAuthenticationError(errMsgId: Int, errString: CharSequence?) {
+                            super.onAuthenticationError(errMsgId, errString)
+                            //dialog?.setStatus(errString?.toString(), R.color.colorError)
+                            authListener.onAuthError(errMsgId, errString ?: "")
+                        }
+
+                        override fun onAuthenticationSucceeded(result: FingerprintManagerCompat.AuthenticationResult?) {
+                            super.onAuthenticationSucceeded(result)
+//                            dialog?.dismiss()
+                            authListener.onAuthSuccess()
+                        }
+
+                        override fun onAuthenticationHelp(helpMsgId: Int, helpString: CharSequence?) {
+                            super.onAuthenticationHelp(helpMsgId, helpString)
+//                            dialog?.setStatus(helpString?.toString())
+                            authListener.onAuthHelp(helpMsgId, helpString ?: "")
+                        }
+
+                        override fun onAuthenticationFailed() {
+                            super.onAuthenticationFailed()
+//                            dialog?.setStatus(context.getString(R.string.fingerprint_not_recognized), R.color.colorError)
+                            authListener.onAuthFailure()
+                        }
+                    }, null)
+            }
+
+            showDialog()
+        }
+    }
+    private fun showDialog() {
+        val title = requireNotNull(builder.title) { "title cannot be null" }
+        val subtitle = requireNotNull(builder.subtitle) { "subTitle cannot be null" }
+        val description = requireNotNull(builder.description) { "description cannot be null" }
+
+//        dialog = BiometricDialog.Builder(context)
+//            .setTitle(title)
+//            .setSubTitle(subtitle)
+//            .setButtonNegative(builder.btnNegativeText)
+//            .setDescription(description)
+//            .build().apply { show() }
+    }
+
+
+
 
     class Builder(private val context: Context) {
         var title: String? = null
@@ -18,8 +150,8 @@ internal class BiometricPrompt(
             private set
         var description: String? = null
             private set
-       // var btnNegativeText: String = context.getString(R.string.cancel)
-      //      private set
+        var btnNegativeText: String = context.getString(R.string.cancel)
+            private set
 
         fun setTitle(title: String): Builder {
             this.title = title
@@ -36,10 +168,11 @@ internal class BiometricPrompt(
             return this
         }
 
-//        fun setButtonNegative(text: String): Builder {
-//            this.btnNegativeText = text
-//            return this
-//        }
+        fun setButtonNegative(text: String): Builder {
+            this.btnNegativeText = text
+            return this
+        }
+
         fun build() = BiometricPrompt(context, this)
     }
 }
